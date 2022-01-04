142587176b0498f93ca9b9ec9d7407e1342eb5b4
looserakki <70113298+looserakki@users.noreply.github.com>
2022-01-04 08:15:07 +0530

    Update chatbot.py

--- lunaBot/modules/chatbot.py
+++ lunaBot/modules/chatbot.py
@@ -3,7 +3,7 @@ import re
 import os
 import html
 import requests
-import lunaBot.modules.sql.kuki_sql as sql
+import lunaBot.modules.sql.chatbot_sql as sql
 
 from time import sleep
 from telegram import ParseMode
